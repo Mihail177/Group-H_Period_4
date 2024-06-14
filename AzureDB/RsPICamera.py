5a894696c22ff4dc59fe088b8c37645b5ed5843f
Mihail177 <mihail.josan@student.nhlstenden.com>
2024-06-14 12:36:30 +0200

    Optimited the code for rasberry app

--- AzureDB/RsPICamera.py
+++ AzureDB/RsPICamera.py
@@ -1,111 +1,119 @@
 import sys
 import socket
+import cv2
 import dlib
+import pigpio
+import os
+from time import sleep
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit
 from PyQt5.QtGui import QPalette, QBrush, QLinearGradient, QColor
 from PyQt5.QtCore import Qt
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import sessionmaker
-from picamera2 import Picamera2, MappedArray
-from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
-import cv2
+from datetime import datetime
+from picamera2 import Picamera2
+
+# Constants
+SERVO_PIN = 18
+SERVO_OPEN_PULSEWIDTH = 2500
+SERVO_CLOSE_PULSEWIDTH = 500
+PIGPIO_DAEMON_START_CMD = "sudo pigpiod"
+PIGPIO_DAEMON_STOP_CMD = "sudo killall pigpiod"
+PI_GPIO_WAIT_TIME = 1
+FACIAL_RECOGNITION_THRESHOLD = 0.6
+DB_CREDENTIALS = {
+    "server": 'facesystemlock.database.windows.net',
+    "database": 'facesystemlock',
+    "username": 'superadmin',
+    "password": 'LKWW8mLOO&amp;qzV0La4NqYzsGmF'
+}
 
 # Paths to the model files
-predictor_path = 'shape_predictor_68_face_landmarks.dat'
-face_rec_model_path = 'dlib_face_recognition_resnet_model_v1.dat'
+PREDICTOR_PATH = 'shape_predictor_68_face_landmarks.dat'
+FACE_REC_MODEL_PATH = 'dlib_face_recognition_resnet_model_v1.dat'
 
 # Initialize the face detector, shape predictor, and face recognition model
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(predictor_path)
-face_rec_model = dlib.face_recognition_model_v1(face_rec_model_path)
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+face_rec_model = dlib.face_recognition_model_v1(FACE_REC_MODEL_PATH)
 
-# Database credentials
-server = 'facesystemlock.database.windows.net'
-database = 'facesystemlock'
-username = 'superadmin'
-password = 'LKWW8mLOO&amp;qzV0La4NqYzsGmF'
-
-# Connection string
-connection_string = f'mssql+pymssql://{username}:{password}@{server}:1433/{database}'
-
-# Create SQLAlchemy engine
+# Create SQLAlchemy engine and session
+connection_string = f"mssql+pymssql://{DB_CREDENTIALS['username']}:{DB_CREDENTIALS['password']}@{DB_CREDENTIALS['server']}:1433/{DB_CREDENTIALS['database']}"
 engine = create_engine(connection_string)
-
-# Define metadata
 metadata = MetaData()
-
-# Reflect the employee table
 employee_table = Table('EMPLOYEE', metadata, autoload_with=engine)
 room_table = Table('ROOM', metadata, autoload_with=engine)
-
-# Create a session
+log_table = Table('LOG', metadata, autoload_with=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(("8.8.8.8", 80))
         ip_address = s.getsockname()[0]
-    finally:
-        s.close()
     return ip_address
+
+class ServoController:
+    def __init__(self, pin, open_pulsewidth, close_pulsewidth):
+        self.pin = pin
+        self.open_pulsewidth = open_pulsewidth
+        self.close_pulsewidth = close_pulsewidth
+        self.pi = pigpio.pi()
+
+    def start(self):
+        os.system(PIGPIO_DAEMON_START_CMD)
+        sleep(PI_GPIO_WAIT_TIME)
+        if not self.pi.connected:
+            raise Exception("Failed to connect to pigpiod")
+
+    def stop(self):
+        self.pi.set_servo_pulsewidth(self.pin, 0)
+        self.pi.stop()
+        os.system(PIGPIO_DAEMON_STOP_CMD)
+
+    def set_servo_pulsewidth(self, pulsewidth):
+        self.pi.set_servo_pulsewidth(self.pin, pulsewidth)
+
+    def open_door(self, open_duration=60):
+        try:
+            self.set_servo_pulsewidth(self.open_pulsewidth)
+            sleep(open_duration)
+            self.set_servo_pulsewidth(self.close_pulsewidth)
+            sleep(2)
+        finally:
+            self.stop()
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Room Registration")
         self.setGeometry(100, 100, 800, 480)
+        self.setup_ui()
+        self.ip_address = get_ip_address()
+        if self.check_room_exists(self.ip_address):
+            self.show_main_menu()
+        self.servo_controller = ServoController(SERVO_PIN, SERVO_OPEN_PULSEWIDTH, SERVO_CLOSE_PULSEWIDTH)
 
-        # Create a gradient background
+    def setup_ui(self):
         palette = QPalette()
         gradient = QLinearGradient(0, 0, 0, 1)
         gradient.setCoordinateMode(QLinearGradient.StretchToDeviceMode)
-        gradient.setColorAt(0.0, QColor(128, 0, 128))  # Purple
-        gradient.setColorAt(1.0, QColor(0, 0, 255))  # Blue
+        gradient.setColorAt(0.0, QColor(128, 0, 128))
+        gradient.setColorAt(1.0, QColor(0, 0, 255))
         palette.setBrush(QPalette.Window, QBrush(gradient))
         self.setPalette(palette)
-
-        self.ip_address = get_ip_address()
-        room_exists = self.check_room_exists(self.ip_address)
-
         self.layout = QVBoxLayout()
-        # Center the layout
         self.layout.setAlignment(Qt.AlignCenter)
-
-        if not room_exists:
-            self.room_input = QLineEdit(self)
-            self.room_input.setPlaceholderText("Enter Room Number")
-            self.room_input.setFixedSize(200, 50)
-            self.room_input.setStyleSheet("background-color: white; font-size: 18px; color: black;")
-            self.layout.addWidget(self.room_input)
-
-            self.submit_button = QPushButton("Submit", self)
-            self.submit_button.setFixedSize(200, 50)
-            self.submit_button.setStyleSheet("background-color: #4CAF50; color: white; font-size: 18px;")
-            self.submit_button.clicked.connect(self.register_room)
-            self.layout.addWidget(self.submit_button)
-
-            self.message_label = QLabel(self)
-            self.message_label.setStyleSheet("font-size: 14px; color: white;")
-            self.message_label.setAlignment(Qt.AlignCenter)
-            self.layout.addWidget(self.message_label)
-        else:
-            self.show_main_menu()
-
         self.setLayout(self.layout)
 
     def check_room_exists(self, ip_address):
-        query = session.query(room_table).filter_by(ip_address=ip_address).first()
-        return query is not None
+        return session.query(room_table).filter_by(ip_address=ip_address).first() is not None
 
     def register_room(self):
         room_number = self.room_input.text()
         if room_number:
-            new_room = room_table.insert().values(room_number=room_number, ip_address=self.ip_address)
-            session.execute(new_room)
+            session.execute(room_table.insert().values(room_number=room_number, ip_address=self.ip_address))
             session.commit()
             self.message_label.setText(f"Room {room_number} registered successfully with IP {self.ip_address}.")
             self.show_main_menu()
@@ -117,77 +125,77 @@ class MainWindow(QWidget):
             widget = self.layout.itemAt(i).widget()
             if widget:
                 widget.setParent(None)
-
-        self.facial_button = QPushButton("Facial Recognition", self)
-        self.facial_button.setFixedSize(200, 50)
-        self.facial_button.setStyleSheet("background-color: #4CAF50; color: white; font-size: 18px;")
-        self.facial_button.clicked.connect(self.run_facial_recognition)
+        self.facial_button = self.create_button("Facial Recognition", self.run_facial_recognition)
         self.layout.addWidget(self.facial_button)
-
-        self.nfc_button = QPushButton("NFC", self)
-        self.nfc_button.setFixedSize(200, 50)
-        self.nfc_button.setStyleSheet("background-color: #008CBA; color: white; font-size: 18px;")
-        # Connect NFC functionality here
+        self.nfc_button = self.create_button("NFC", self.run_nfc)
         self.layout.addWidget(self.nfc_button)
+        self.recognition_label = QLabel(self)
+        self.recognition_label.setStyleSheet("font-size: 18px; color: white;")
+        self.recognition_label.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.recognition_label)
+
+    def create_button(self, text, handler):
+        button = QPushButton(text, self)
+        button.setFixedSize(200, 50)
+        button.setStyleSheet("background-color: #4CAF50; color: white; font-size: 18px;")
+        button.clicked.connect(handler)
+        return button
+
+    def run_nfc(self):
+        # Implement NFC functionality here
+        pass
 
     def run_facial_recognition(self):
-        # Prepare known face descriptors
-        known_face_descriptors = []
-        known_names = []
+        known_face_descriptors, known_names, known_employee_ids = self.load_known_faces()
+        frame = self.capture_image()
+        if frame is None:
+            self.recognition_label.setText("Failed to capture image.")
+            return
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+        if len(faces) == 0:
+            self.recognition_label.setText("No face detected.")
+            return
+        matched_name, matched_employee_id = self.recognize_face(faces[0], frame, known_face_descriptors, known_names, known_employee_ids)
+        self.recognition_label.setText(f"Recognized: {matched_name}")
+        if matched_employee_id:
+            self.log_access(matched_employee_id)
+            self.servo_controller.open_door()
 
-        employees = session.query(employee_table).all()
-        for employee in employees:
-            face_descriptor_blob = employee.facial_data
-            face_descriptor = np.frombuffer(face_descriptor_blob, dtype=np.float64)  # Adjust this line based on how your data is stored
+    def load_known_faces(self):
+        known_face_descriptors, known_names, known_employee_ids = [], [], []
+        for employee in session.query(employee_table).all():
+            face_descriptor = np.frombuffer(employee.facial_data, dtype=np.float64)
             known_face_descriptors.append(face_descriptor)
-            full_name = f"{employee.first_name} {employee.last_name}"  # Concatenate first and last name
-            known_names.append(full_name)
+            known_names.append(f"{employee.first_name} {employee.last_name}")
+            known_employee_ids.append(employee.employee_id)
+        return known_face_descriptors, known_names, known_employee_ids
 
-        # Initialize Picamera2
+    def capture_image(self):
         picam2 = Picamera2()
-        config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)})
-        picam2.configure(config)
+        picam2.configure(picam2.create_still_configuration())
         picam2.start()
-
-        while True:
-            frame = picam2.capture_array()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = detector(gray)
-
-            for face in faces:
-                landmarks = predictor(gray, face)
-                face_descriptor = face_rec_model.compute_face_descriptor(frame, landmarks)
-                np_face_descriptor = np.array(face_descriptor)
-
-                # Compare with known face descriptors
-                min_distance = float('inf')
-                matched_name = "Unknown"
-
-                for i, known_face_descriptor in enumerate(known_face_descriptors):
-                    distance = np.linalg.norm(known_face_descriptor - np_face_descriptor)
-                    if distance < min_distance:
-                        min_distance = distance
-                        matched_name = known_names[i]
-
-                # Set a threshold for considering a face as recognized (e.g., 0.6)
-                if min_distance < 0.6:
-                    name = matched_name
-                    print(f"Face matches with distance {min_distance:.2f}. You now have access to the room, {name}.")
-                else:
-                    name = "Unknown"
-                    print("Face does not match. Access denied.")
-
-                # Draw a rectangle around the face and label it
-                x, y, w, h = (face.left(), face.top(), face.width(), face.height())
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-            cv2.imshow('Facial Recognition', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
+        frame = picam2.capture_array()
         picam2.stop()
-        cv2.destroyAllWindows()
+        return frame
+
+    def recognize_face(self, face, frame, known_face_descriptors, known_names, known_employee_ids):
+        landmarks = predictor(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), face)
+        face_descriptor = np.array(face_rec_model.compute_face_descriptor(frame, landmarks))
+        min_distance, matched_name, matched_employee_id = float('inf'), "Unknown", None
+        for known_face_descriptor, name, emp_id in zip(known_face_descriptors, known_names, known_employee_ids):
+            distance = np.linalg.norm(known_face_descriptor - face_descriptor)
+            if distance < min_distance:
+                min_distance, matched_name, matched_employee_id = distance, name, emp_id
+        if min_distance >= FACIAL_RECOGNITION_THRESHOLD:
+            matched_name, matched_employee_id = "Unknown", None
+        return matched_name, matched_employee_id
+
+    def log_access(self, employee_id):
+        room = session.query(room_table).filter_by(ip_address=self.ip_address).first()
+        if room:
+            session.execute(log_table.insert().values(employee_id=employee_id, room_number=room.room_number, date_and_time=datetime.now()))
+            session.commit()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
